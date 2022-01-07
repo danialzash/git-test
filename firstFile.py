ff3a2873ea06508c06b90c82370aadded15e87aa
danialzash <40217623+danialzash@users.noreply.github.com>
2022-01-08 00:05:50 +0330

    add lambda function to firstFile.py

--- firstFile.py
+++ firstFile.py
@@ -1,8 +1,15 @@
 
-'''
+"""
 first file to work with git
-'''
+"""
 
+def my_func(n):
+  return lambda a : a * n
+
+double_func = my_func(2)
+triple_func = my_func(3)
+
+print(double_func(10), triple_func(10))
 
 print('Hello, World!') #some comment
 
